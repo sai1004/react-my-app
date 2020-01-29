5b5b9779e920c2429cac6180ce4243adfef53801
sai1004 <saikiran.bavandla@gmail.com>
2020-01-29 16:47:09 +0530

    commmit

--- src/Application.js
+++ src/Application.js
@@ -5,7 +5,8 @@ class Application extends Component {
     super(props);
 
     this.state = {
-      count: 0
+      count: 0,
+      overTen: false
     };
   }
 
@@ -38,10 +39,12 @@ class Application extends Component {
   };
 
   componentDidUpdate(props, state) {
-    //  if (this)
+    if (this.state.count > 10 && this.state.count != state.count && !this.state.overTen) {
+        console.log('Updating OverTen')
+      this.setState({ overTen: true });
+    }
     // console.log("Updated from", state, "to", this.state)
   }
-
 
   render() {
     // let name = "Nick ";
@@ -50,7 +53,9 @@ class Application extends Component {
     return (
       <div>
         {/* <h1> Hello, {name} </h1> */}
-        <h1> You Clicked the {count} button </h1>
+        <h1> You Clicked the button {count} Times </h1>
+
+        {this.state.overTen ? <h3> Beat High Score Of 10</h3> : null}
         <span>
           <button onClick={e => this.handelClick()}> Click Me </button>
         </span>
